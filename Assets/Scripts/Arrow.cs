a74a742a057246db70bf4e8c2f775be400cc8b89
Mathieu <mathieu.herranzperez@gmail.com>
2019-05-17 12:04:44 -0400

    Bow (a tester)

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,10 +1,66 @@
 ﻿using UnityEngine;
+using Valve.VR;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Arrow : MonoBehaviour
 {
+    public SteamVR_Action_Single squeezeAction;
+
+    // ---- INTERN ----
+    private bool isAttached = false;
+    private bool isFired = false;
+    private Rigidbody rBody;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (isFired)
+            FollowGravity();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        AttachArrow();
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        ArrowManager.Instance.AttachBowToArrow();
+        AttachArrow();
+    }
+
+    public void Fire(Vector3 force)
+    {
+        isFired = true;
+        rBody.velocity = force;
+        rBody.useGravity = true;
+    }
+
+
+    private void FollowGravity()
+    {
+        float angle = Mathf.Atan2(rBody.velocity.y, rBody.velocity.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    //https://www.youtube.com/watch?v=bn8eMxBcI70
+    private void AttachArrow()
+    {
+        // right hand /!\
+        if(SteamVR_Input.GetBooleanAction("GrabGrip").GetState(SteamVR_Input_Sources.RightHand)) // Any ?
+        {
+            Debug.Log("GrabGrip down");
+        }
+
+        float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.RightHand);
+        if(!isAttached && triggerValue > 0.05f)
+        {
+            isAttached = true;
+            Debug.Log("Squeezed : " + triggerValue);
+            ArrowManager.Instance.AttachBowToArrow();
+        }
     }
 }
--- Assets/Scripts/ArrowManager.cs
+++ Assets/Scripts/ArrowManager.cs
@@ -6,20 +6,24 @@ public class ArrowManager : MonoBehaviour
     public static ArrowManager Instance { get; private set; }
 
     [Header("Setup")]
+    public SteamVR_Action_Single squeezeAction;
     [SerializeField]
     private GameObject arrowPrefab = default;
     [SerializeField]
-    private Transform stringAttachTransform;
+    private GameObject stringAttachPointGO = default;
     [SerializeField]
     private Transform arrowStartPoint = default;
     [SerializeField]
     private Vector3 spawnPoint = new Vector3(0f, 0f, 0.34f);
+    [SerializeField]
+    private Transform stringStartPoint = default;
 
 
     public SteamVR_Behaviour_Pose trackedObject;
 
     // ---- INTERN ----
     private GameObject currentArrow;
+    private bool isAttached = false;
 
     void Awake()
     {
@@ -36,6 +40,7 @@ public class ArrowManager : MonoBehaviour
     {
         // todo modifier pour moins moche
         AttachArrow();
+        PullString();
     }
 
     private void AttachArrow()
@@ -43,15 +48,45 @@ public class ArrowManager : MonoBehaviour
         if(currentArrow == null)
         {
             // todo retirer fleche de la main dans le hierarchie
-            currentArrow = (GameObject)Instantiate(arrowPrefab, transform.position, transform.rotation, trackedObject.transform);
+            currentArrow = (GameObject)Instantiate(arrowPrefab, transform.position, Quaternion.identity, trackedObject.transform);
             currentArrow.transform.localPosition = spawnPoint;
         }
     }
 
+    private void PullString()
+    {
+        if(isAttached)
+        {
+            float dist = Vector3.Distance(stringStartPoint.position, trackedObject.transform.position);
+            stringAttachPointGO.transform.localPosition = stringStartPoint.transform.localPosition + new Vector3(5f * dist, 0f, 0f);
+
+            float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.RightHand);
+            if (triggerValue <= 0.05f)
+            {
+                Debug.Log("LOOSE : " + triggerValue);
+                Fire();
+
+                stringAttachPointGO.transform.position = stringStartPoint.position;
+                currentArrow = null;
+                isAttached = false;
+            }
+        }
+    }
+
+    private void Fire()
+    {
+        currentArrow.transform.parent = null;
+
+        Vector3 force = currentArrow.transform.forward * 10f;
+        currentArrow.GetComponent<Arrow>().Fire(force);
+    }
+
     public void AttachBowToArrow()
     {
-        currentArrow.transform.parent = stringAttachTransform;
+        currentArrow.transform.parent = stringAttachPointGO.transform;
         currentArrow.transform.localPosition = arrowStartPoint.localPosition;
         currentArrow.transform.rotation = arrowStartPoint.rotation;
+
+        isAttached = true;
     }
 }
